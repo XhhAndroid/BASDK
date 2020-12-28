980c91aa5e8b99e5a5bd979816c923eefe4c7059
zhangxiaohui <809887981@qq.com>
2020-12-28 15:17:44 +0800

    修复更新socket未添加配置

--- contract/src/main/java/com/bkt/contract/ba/common/TickerDtoToPairInfoPoListFunction.kt
+++ contract/src/main/java/com/bkt/contract/ba/common/TickerDtoToPairInfoPoListFunction.kt
@@ -1,22 +1,32 @@
 package com.bkt.contract.ba.common
 
+import com.bkt.contract.ba.enums.ContractType
 import com.bkt.contract.ba.model.dto.TickerEventDto
 import com.bkt.contract.ba.model.po.PairInfoPo
+import com.bkt.contract.ba.service.inner.PairDbService
 import io.reactivex.functions.Function
 
 /**
  * @Description: ticker dto转pairinfo po
-   * @Author: XGod  xuanyouwu@163.com  17611639080  https://github.com/NBXXF     https://blog.csdn.net/axuanqq
+ * @Author: XGod  xuanyouwu@163.com  17611639080  https://github.com/NBXXF     https://blog.csdn.net/axuanqq
  * @CreateDate: 2020/12/10 10:01
  */
 internal class TickerDtoToPairInfoPoListFunction : Function<List<TickerEventDto>, List<PairInfoPo>> {
     override fun apply(t: List<TickerEventDto>): List<PairInfoPo> {
-        val list: MutableList<PairInfoPo> = mutableListOf();
-        for (item: TickerEventDto in t) {
-            val pairInfoPo = PairInfoPo(item.symbol);
-            pairInfoPo.ticker = item;
-            list.add(pairInfoPo);
-        }
-        return list;
+        return PairDbService.getPairConfig()
+                .map { pairCacheMap ->
+                    val list: MutableList<PairInfoPo> = mutableListOf()
+
+                    for (item: TickerEventDto in t) {
+                        val pairInfoPo = PairInfoPo(item.symbol);
+                        pairInfoPo.ticker = item
+
+                        val cacheConfig = pairCacheMap[pairInfoPo.symbol]
+                        pairInfoPo.contractType = cacheConfig?.contractClassifyType?.value ?:ContractType.USDT.value
+
+                        list.add(pairInfoPo)
+                    }
+                    list;
+                }.blockingFirst()
     }
 }
